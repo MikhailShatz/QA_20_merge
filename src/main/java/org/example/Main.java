0d90bcc9854d37175e1ea27e19b0fdaa559061bf
MikhailShatz <Tipavideo@gmail.com>
2023-09-03 19:34:24 +0300

    add loop and str

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,9 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        for (int j = 0; j< 10; j++) {
+            System.out.println("Wee-wee");
+        }
         System.out.println("Bonk");
     }
 }
